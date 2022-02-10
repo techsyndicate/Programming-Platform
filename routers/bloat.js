b60e55e07d481fad8961dd2977fce78fa7209748
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-10 16:45:57 +0530

    Routers: BloatWare Router Create File
    1. This is not a router, but is needed to add some important packages to
       the code. this is in a way a hack to section off code so that it
       doesn't clutter the index/server.js file.

---
+++ routers/bloat.js
@@ -0,0 +1,48 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
+
+const bloat_router = express.Router();
+
+//serve public folder
+bloat_router.use(express.static('public'));
+
+//cors middleware
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            console.log(origin)
+            console.log("Not allowed by CORS ORIGIN policy")
+            callback(new Error("Not allowed by CORS ORIGIN: "+origin))
+        }
+    },
+    credentials: true
+}
+bloat_router.use(cors(corsOptions))
+
+// Allowed hosts
+const allowedHosts = ['localhost', 'syntax-prelims.herokuapp.com'];
+const checkHosts = (req, res, next) => {
+    if (allowedHosts.includes(req.hostname)) {
+        return next();
+    }
+    return res.sendStatus(403);
+}
+bloat_router.use(checkHosts);
+
+//body parsers 
+bloat_router.use(bodyParser.json({
+    parameterLimit: 100000,
+    limit: '50mb'
+}));
+bloat_router.use(bodyParser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}));
+
+//export routers
+module.exports = bloat_router;
