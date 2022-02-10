a5f1ef599e99ef262f3c314648d471bb7e3cbbeb
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-10 16:47:44 +0530

    index.js: Main File Create File
    1. Add MongoDb, Connected Various Routers, and other misc stuff.

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    session = require("cookie-session"),
+    cookieParser = require("cookie-parser"),
+    passport = require('passport');
+
+const port = process.env.PORT || 3200,
+    passport_init = require('./passport'),
+    bloatRouter = require('./routers/bloat'),
+    authRouter = require("./routers/auth");
+
+//mongo
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+const MONGO_USER = process.env.MONGO_USER
+const MONGO_CLUSTER_URL = process.env.MONGO_CLUSTER_URL
+const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME
+const db = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER_URL}/${MONGO_DATABASE_NAME}?retryWrites=true&w=majority`
+
+//passportJs
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode1"));
+
+passport_init(passport);
+
+//initializing passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(bloatRouter);
+app.use("/auth", authRouter)
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to Mongo DB")
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}).catch(err => console.log(err))
