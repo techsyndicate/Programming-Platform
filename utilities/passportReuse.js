f03bdb6944fce1c6d2f8969d6f6c5d21752ba755
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-10 16:43:24 +0530

    Utilities: PassportJs Reusables Create File
    1. Add CheckAuthenticated And NotAuthenticated Functions.

---
+++ utilities/passportReuse.js
@@ -0,0 +1,16 @@
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect(`/dashboard`);
+    }
+    next();
+}
+
+module.exports = { checkAuthenticated, checkNotAuthenticated }
