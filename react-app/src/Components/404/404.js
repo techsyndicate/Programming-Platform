873f8b97ea2f515021f9483ce4f0291fd142a74d
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-20 15:26:47 +0530

    ReactJS: Update 404 Page

--- react-app/src/Components/404/404.js
+++ react-app/src/Components/404/404.js
@@ -16,7 +16,7 @@ function fourzerofour() {
                             <h3>Oops! Page not found</h3>
                             <h1><span>4</span><span>0</span><span>4</span></h1>
                         </div>
-                        <h2>we are sorry, but the page you requested was not found</h2>
+                        <h2>We are sorry <strike>Are We?</strike>, but the page you requested was not found</h2>
                     </div>
                 </div>
 
