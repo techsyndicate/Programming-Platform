3f5d6784a7d226c0f73212031f9ac1cfef0229c8
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-28 12:20:12 +0530

    SCHEMAS: MISC: UTILITIES: Update UserSchema+Misc Utility
    1. Update User Schema For Discord
    2. Update Utitlities to handle Discord

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -7,6 +7,8 @@ const reqString = { type: String, required: true },
     reqStringFalseDefEmpty = { type: String, required: false, default: "" },
     reqStringFalse = { type: String, required: false},
     reqBool = { type: Boolean, required: true, default: false },
+    reqBoolFalse = { type: Boolean, required: false, default: false },
+
     dateStringWithTime = moment(new Date()).format('YYYY-MM-DD HH:MM:SS');
 
 // Schema
@@ -26,9 +28,11 @@ const userSchema = new mongoose.Schema({
         expires_in: reqStringFalse,
         refresh_token: reqStringFalse,
         scope: reqStringFalse,
-        token_type: reqStringFalse
+        token_type: reqStringFalse,
+        ISSUED_AT: reqStringFalse
     },
     discordUser: {
+        ISSUED_AT: reqStringFalse,
         id: reqStringFalse,
         username: reqStringFalse,
         avatar: reqStringFalse,
@@ -41,7 +45,7 @@ const userSchema = new mongoose.Schema({
         locale: reqStringFalse,
         mfa_enabled: reqStringFalse,
         email: reqStringFalse,
-        verified: reqBool
+        verified: reqBoolFalse
     }
 })
 
--- utilities/misc.js
+++ utilities/misc.js
@@ -20,5 +20,19 @@ async function getDiscordUser(access_token) {
     });
 }
 
+async function refreshDiscordToken(refresh_token) {
+    return await Axios({
+        url: `https://discord.com/api/oauth2/token`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: `client_id=${process.env.DISCORD_CLIENT_ID}&client_secret=${process.env.DISCORD_CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${refresh_token}`
+    }).then((res_dis) => {
+        return res_dis.data;
+    }).catch((err) => {
+        return err;
+    });
+}
 
-module.exports = { validateEmail, getDiscordUser }
+module.exports = { validateEmail, getDiscordUser, refreshDiscordToken }
