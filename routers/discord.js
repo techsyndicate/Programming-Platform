203e86f6018130e22e0d7541dec959666f7209fd
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-28 12:18:55 +0530

    Routers+Public: Update Discord.js With Avatar Support etc.
    1. Add Avatar Support using res.redirect

--- routers/discord.js
+++ routers/discord.js
@@ -5,16 +5,20 @@ const express = require('express'),
 const Axios = require('axios');
 const userSchema = require('../schema/userSchema');
 const { checkAuthenticated } = require('../utilities/passportReuse');
-const { getDiscordUser } = require('../utilities/misc');
+const { getDiscordUser, refreshDiscordToken } = require('../utilities/misc');
+const fs = require('fs');
 
 var scopes = ['identify', 'email', 'guilds', 'guilds.join', 'guilds.members.read', 'gdm.join'];
 var prompt = 'none';
 
 discord_router.get('/auth', checkAuthenticated, (req, res) => {
-    // Redirect To Discord OAuth2
-    // https://discord.com/developers/docs/topics/oauth2#authorization-code-grant
-
-    res.redirect(`https://discord.com/api/oauth2/authorize?response_type=code&client_id=${process.env.DISCORD_CLIENT_ID}&scope=${encodeURI(scopes.join(' '))}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&prompt=${prompt}`);
+    if (req.user.discord.access_token) {
+        res.redirect('/profile');
+    } else {
+        // Redirect To Discord OAuth2
+        // https://discord.com/developers/docs/topics/oauth2#authorization-code-grant
+        res.redirect(`https://discord.com/api/oauth2/authorize?response_type=code&client_id=${process.env.DISCORD_CLIENT_ID}&scope=${encodeURI(scopes.join(' '))}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&prompt=${prompt}`);
+    }
 });
 
 discord_router.get('/callback', checkAuthenticated, async (req, res, next) => {
@@ -31,8 +35,10 @@ discord_router.get('/callback', checkAuthenticated, async (req, res, next) => {
             data: `client_id=${process.env.DISCORD_CLIENT_ID}&client_secret=${process.env.DISCORD_CLIENT_SECRET}&grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}`
         }).then(async (res_dis) => {
             userSchema.findById(req.user.id).then(async (user) => {
+                res_dis.data.ISSUED_AT = new Date().toString();
                 user.discord = res_dis.data;
                 user.discordUser = await getDiscordUser(res_dis.data.access_token)
+                user.discordUser.ISSUED_AT = new Date().toString();
                 user.save().then(() => {
                     res.redirect('/Profile');
                 });
@@ -49,8 +55,62 @@ discord_router.get('/current-user', checkAuthenticated, async (req, res, next) =
     // https://discord.com/api/users/@me
     // https://discord.com/developers/docs/game-sdk/users#getcurrentuser
 
-    res.send(await getDiscordUser(req.user.discord.access_token));
+    if (req.user.discord.access_token) {
+        var data = await getDiscordUser(req.user.discord.access_token);
+        if (data.toJSON) {
+            data = JSON.parse(JSON.stringify(data.toJSON()))
+        }
+        if (data.status && data.status == 401) {
+            // refresh token
+            var refresh_token = req.user.discord.refresh_token;
+            var refresh_data = await refreshDiscordToken(refresh_token);
+            userSchema.findById(req.user.id).then(async (user) => {
+                refresh_data.ISSUED_AT = new Date().toString();
+                user.discord = refresh_data;
+                user.discordUser = await getDiscordUser(refresh_data.access_token)
+                user.discordUser.ISSUED_AT = new Date().toString();
+                user.save().then(() => {
+                    res.send(user.discordUser);
+                });
+            });
+        }
+        else {
+            userSchema.findById(req.user.id).then(async (user) => {
+                user.discordUser = data;
+                user.discordUser.ISSUED_AT = new Date().toString();
+                user.save().then(() => {
+                    res.send(data);
+                });
+            });
+        }
+    } else {
+        res.send({ success: false });
+    }
+})
 
+discord_router.get('/avatar', checkAuthenticated, (req, res, next) => {
+    // get avatar from discord
+
+    if (req.user.discord.access_token) {
+        if (Math.abs(new Date() - req.user.discordUser.ISSUED_AT) > 3600000) {
+            // refresh token
+            var refresh_token = req.user.discord.refresh_token;
+            var refresh_data = refreshDiscordToken(refresh_token);
+            userSchema.findById(req.user.id).then(async (user) => {
+                refresh_data.ISSUED_AT = new Date().toString();
+                user.discord = refresh_data;
+                user.discordUser = await getDiscordUser(refresh_data.access_token)
+                user.discordUser.ISSUED_AT = new Date().toString();
+                user.save().then(() => {
+                    res.redirect('https://cdn.discordapp.com/avatars/' + user.discordUser.id + '/' + user.discordUser.avatar + '.png?size=1024&quality=lossless');
+                });
+            });
+        } else {
+            res.redirect('https://cdn.discordapp.com/avatars/' + req.user.discordUser.id + '/' + req.user.discordUser.avatar + '.png?size=1024&quality=lossless');
+        }
+    } else {
+        res.redirect('/images/defaultdis.png');
+    }
 })
 
 module.exports = discord_router;
