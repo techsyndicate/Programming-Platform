9b0951b275c3392274126e3c4057b2a291d97d0d
Aayush Garg <aayushgarg2006@gmail.com>
2022-03-02 17:58:00 +0530

    ReactJS: event.js Update, BugFix i.e ui fix

--- react-app/src/Pages/Event/Event.js
+++ react-app/src/Pages/Event/Event.js
@@ -44,10 +44,10 @@ function Event() {
                       <Button
                         onClick={() => {
                           let userData = JSON.parse(localStorage.getItem('User'));
-                          console.log(userData.discordUser.verified && userData.emailVerified)
                           if (!checkLoggedIn()) {
                             notyf.error('You must be logged in to Participate In an event');
-                          } else if (!(userData.discordUser.verified && userData.emailVerified)) {
+                          }
+                          else if (!(userData.discordUser.verified && userData.emailVerified)) {
                             notyf.error('You must Complete Your Profile, i.e Verify Your Email On Discord and On Our platform, and Link Discord To Participate In Events');
                           } else {
                             window.location.href = '/Events/' + item.event.name
